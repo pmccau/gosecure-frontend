e0168195767c7214a27da3f07afb349e4c5165ac
Pat <pmccau@upenn.edu>
2021-01-02 14:47:46 -0500

    PPPD working

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,6 +1,192 @@
 import React from 'react';
 import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
-import DogTracker from "./DogTracker";
+import Modal from 'react-modal'
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
+
+/**
+ * The DogTracker class is used to track when a dog has last gone out
+ */
+class DogTracker extends React.Component {
+
+    /**
+     * Constructor for the DogTracker modal object
+     * @param props
+     */
+    constructor (props) {
+        super(props);
+        this.state = {
+            showModal: false,
+            iconName: "paw-orange",
+            status: this.getStatus(),
+        };
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
+    /**
+     * Handle the opening of the modal
+     */
+    handleOpenModal () {
+        this.setState({ showModal: true });
+    }
+
+    /**
+     * Handle the closing of the modal
+     */
+    handleCloseModal () {
+        this.setState({ showModal: false });
+    }
+
+    /**
+     * Log a movement
+     * @param number the movement number, either 1 or 2
+     */
+    logMovement (number) {
+        const response = fetch("http://localhost:5000/api/movement/" + number, {
+            method: 'POST',
+        })
+            .then(() => this.getStatus())
+    }
+
+    /**
+     * Get the status from the database for movements
+     */
+    getStatus () {
+        fetch("http://localhost:5000/api/movement", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            // .then(res => console.log(res.json()))
+            .then(res => res.json())
+            .then(json => {
+                // console.log(json)
+                this.setState({
+                    status: json,
+                })
+            })
+    }
+
+    /**
+     * Helper function to determine the time difference between two date objects
+     * @param d1 Date object 1
+     * @param d2 Date object 2
+     * @returns {string} d1 - d2 formatted as HH:MM
+     */
+    getTimeDifference(d1, d2) {
+        const diff = (d1.getTime() - d2.getTime()) / 1000 / 60;
+        const hours = parseInt(diff / 60)
+        const minutes = parseInt(diff)
+        const seconds = Math.round((diff - minutes) * 60)
+        console.log(seconds)
+        return (("" + hours).length == 1 ? "0" + hours : hours) + ":" + (("" + minutes).length == 1 ? "0" + minutes : minutes)
+    }
+
+    /**
+     * Get the time since most recent movement 1 occurred
+     * @returns {string}
+     */
+    getLastPee () {
+        if (!this.state.status) {
+            return "N/A"
+        }
+        const date = new Date(new Date(this.state.status[0].Timestamp).getTime() - (5*60*60000));
+        const now = new Date();
+        return this.getTimeDifference(now, date);
+    }
+
+    /**
+     * Get the time since most recent movement 2 occurred
+     * @returns {string}
+     */
+    getLastPoo () {
+        if (!this.state.status) {
+            return "N/A"
+        }
+        const date = new Date(new Date(this.state.status[1].Timestamp).getTime() - (5*60*60000));
+        const now = new Date();
+        return this.getTimeDifference(now, date);
+    }
+
+    /**
+     * Get the layout for status from the server
+     */
+    showStatus () {
+        return (
+            <div>
+                <table><tbody><tr>
+                        <td className="widgetMainText">&nbsp;&nbsp;{this.getLastPee()}&nbsp;&nbsp;</td>
+                        <td className="widgetMainText">&nbsp;&nbsp;{this.getLastPoo()}&nbsp;&nbsp;</td>
+                    </tr><tr>
+                        <td><button onClick={() => this.logMovement(1)} className="sensorButton sensorButtonText">
+                            <img width="50px" height="50px"
+                                src={process.env.PUBLIC_URL + "move1.png"}
+                            />
+                        </button></td>
+                        <td><button onClick={() => this.logMovement(2)} className="sensorButton sensorButtonText">
+                            <img width="50px" height="50px"
+                                 src={process.env.PUBLIC_URL + "move2.png"}
+                            />
+                        </button></td>
+                </tr><tr><td colSpan="2">
+                    <button onClick={() => {
+                        this.logMovement(1);
+                        this.logMovement(2)
+                    }} className="sensorButton sensorButtonText">
+                        <img width="50px" height="50px"
+                             src={process.env.PUBLIC_URL + "move1.png"}
+                        />
+                        <img width="50px" height="50px"
+                             src={process.env.PUBLIC_URL + "move2.png"}
+                        />
+                    </button>
+                </td></tr><tr><td>
+                    <button onClick={this.handleCloseModal} className="sensorButton sensorButtonText">
+                        cancel
+                    </button>
+                </td></tr></tbody></table>
+            </div>
+        )
+    }
+
+    /**
+     * Render the modal
+     * @returns {JSX.Element}
+     */
+    render () {
+        return (
+            <div>
+                <button className="navbutton" onClick={() => {
+                    this.handleOpenModal();
+                    console.log(this.getStatus())
+                }}>
+                    <img
+                        className={this.props.className}
+                        src={process.env.PUBLIC_URL + this.state.iconName + ".png"}
+                    />
+                </button>
+                <Modal
+                    isOpen={this.state.showModal}
+                    contentLabel="Minimal Modal Example"
+                    style={customStyles}
+                >
+                    {this.showStatus()}
+                </Modal>
+            </div>
+        );
+    }
+}
 
 class Lock extends React.Component {
     constructor(props) {
@@ -26,44 +212,27 @@ class Lock extends React.Component {
 
     render() {
         return (
-            <button
-                className="navbutton"
-                onClick={this.onClick}
-            >
-                <img
-                    className={this.props.className}
-                    src={process.env.PUBLIC_URL + this.state.iconName + ".png"}
-                />
-            </button>
+            <div>
+                <button
+                    className="navbutton"
+                    onClick={this.onClick}
+                >
+                    <img
+                        className={this.props.className}
+                        src={process.env.PUBLIC_URL + this.state.iconName + ".png"}
+                    />
+                </button>
+            </div>
         );
     }
 }
 
 export const NavigationBar = () => (
     <Navbar expand="lg">
-        {/*<DogTracker />*/}
-        <Lock
-            className="navicon"
-        />
-        <DogTracker
-            className="navicon"
-        />
-
-        {/*<a href={"/"}>*/}
-        {/*    <img*/}
-        {/*        className="navicon"*/}
-        {/*        src={process.env.PUBLIC_URL + "paw-orange" + ".png"}*/}
-        {/*    />*/}
-        {/*</a>*/}
-        {/*<Navbar.Brand href="/">*/}
-
-        {/*</Navbar.Brand>*/}
-
-        {/*<Navbar.Collapse id="basic-navbar-nav">*/}
-        {/*    <Nav className="ml-auto">*/}
-        {/*        /!*<Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>*!/*/}
-        {/*        /!*<Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>*!/*/}
-        {/*    </Nav>*/}
-        {/*</Navbar.Collapse>*/}
+        <table><tr><td className="button-cell">
+            <Lock className="navicon" />
+        </td><td className="button-cell">
+            <DogTracker className="navicon" />
+        </td></tr></table>
     </Navbar>
 )
